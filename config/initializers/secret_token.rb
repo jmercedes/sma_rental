dbd8a45ef3d01be407a7aa19c3fd7328ad67f45c
Juan Mercedes <j.mercedes@thedesignpill.com>
2013-07-09 07:52:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmaRental::Application.config.secret_token = '7c4fc30c7aa740d4a9dc034f78ba1ed7492962ffbf01aeb5bd2dadc6e9781b1ecdaeff70acd1d9d79055119c10105e3f4cd7d6b732cd673d8d582b713605bf34'
